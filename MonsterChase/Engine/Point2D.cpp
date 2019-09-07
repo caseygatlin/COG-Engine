b3f63a73c40873fb199b53fb9ca8acec034c23c6
Casey Gatlin <u1274838@utah.edu>
2019-09-06 22:19:36 -0600

    Solidified Point2D class

--- MonsterChase/Engine/Point2D.cpp
+++ MonsterChase/Engine/Point2D.cpp
@@ -3,33 +3,128 @@
 namespace Engine
 {
 	//Constructor
-	Point2D::Point2D(float x, float y)
+	Point2D::Point2D(int x, int y)
 	{
 		m_x = x;
 		m_y = y;
+	}
+
+	//Copy constructor
+	Point2D::Point2D(const Point2D& src)
+	{
+		m_x = src.m_x;
+		m_y = src.m_y;
+	}
+
+	//Assignment operator
+	Point2D& Point2D::operator=(const Point2D& src)
+	{
+		if (&src == this)
+			return (*this);
+
+		m_x = src.m_x;
+		m_y = src.m_y;
+
+		return (*this);
+	}
+
+	//Addition
+	Point2D Point2D::operator+(const Point2D rhs)
+	{
+		int x = m_x + rhs.m_x;
+		int y = m_y + rhs.m_y;
+		Point2D p(x, y);
+		return p;
+	}
+
+	//Subtraction
+	Point2D Point2D::operator-(const Point2D rhs)
+	{
+		int x = m_x - rhs.m_x;
+		int y = m_y - rhs.m_y;
+		Point2D p(x, y);
+		return p;
+	}
+
+	//Prefix increment
+	Point2D& Point2D::operator++()
+	{
+		++m_x;
+		++m_y;
+		return (*this);
+	}
+
+	//Postfix increment
+	Point2D Point2D::operator++(int)
+	{
+		int x = m_x++;
+		int y = m_y++;
+		Point2D p(x, y);
+		return p;
+	}
+
+	//Prefix decrement
+	Point2D& Point2D::operator--()
+	{
+		--m_x;
+		--m_y;
+		return (*this);
+	}
+
+	//Postfix decrement
+	Point2D Point2D::operator--(int)
+	{
+		int x = m_x--;
+		int y = m_y--;
+		Point2D p(x, y);
+		return p;
+	}
+
+
+	//Equal to
+	bool Point2D::operator==(Point2D const& rhs) const
+	{
+		return (m_x == rhs.m_x && m_y == rhs.m_y);
+	}
+
+	//Not equal to
+	bool Point2D::operator!=(Point2D const& rhs)
+	{
+		return !(m_x == rhs.m_x && m_y == rhs.m_y);
+	}
+
+	//Addition assignment
+	Point2D& Point2D::operator+=(Point2D rhs)
+	{
+		m_x += rhs.m_x;
+		m_y += rhs.m_y;
+		return (*this);
+	}
+
+	//Subtraction assignment
+	Point2D& Point2D::operator-=(Point2D rhs)
+	{
+		m_x -= rhs.m_x;
+		m_y -= rhs.m_y;
+		return (*this);
+	}
+
+	//Ostream <<
+	std::ostream& operator<<(std::ostream& out, const Point2D& point)
+	{
+		out << "[" << point.m_x << ", " << point.m_y << "]";
+		return out;
 	}
 
 
 	//Getters
-	float Point2D::getX() const
+	int Point2D::X() const
 	{
 		return m_x;
 	}
 
-	float Point2D::getY() const
+	int Point2D::Y() const
 	{
 		return m_y;
 	}
-
-	//Setters
-	void Point2D::setX(float x)
-	{
-		m_x = x;
-	}
-
-	void Point2D::setY(float y)
-	{
-		m_y = y;
-	}
-
 }
--- MonsterChase/Engine/Point2D.h
+++ MonsterChase/Engine/Point2D.h
@@ -1,27 +1,59 @@
 #ifndef POINT2D_H
 #define POINT2D_H
+#include <iostream>
 
 namespace Engine
 {
+	//Class for an X,Y coordinate
 	class Point2D
 	{
 	public:
 		//Constructor
-		Point2D(float x = 0, float y = 0);
+		Point2D(int x = 0, int y = 0);
+
+		//Copy constructor
+		Point2D(const Point2D& src);
+
+		//Assignment operator
+		Point2D& operator=(const Point2D& src);
+
+		//Addition
+		Point2D operator+(const Point2D rhs);
+
+		//Subtraction
+		Point2D operator-(const Point2D rhs);
+
+		//Increment
+		Point2D& operator++();
+		Point2D operator++(int);
+
+		//Decrement
+		Point2D& operator--();
+		Point2D operator--(int);
+
+		//Equal to
+		bool operator==(Point2D const& rhs) const;
+
+		//Not equal to
+		bool operator!=(Point2D const& rhs);
+
+		//Addition assignment
+		Point2D& operator+=(Point2D rhs);
+
+		//Subtraction assignment
+		Point2D& operator-=(Point2D rhs);
+
+		//Ostream <<
+		friend std::ostream& operator<<(std::ostream& out, const Point2D& point);
 		
 		//Getters
-		float getX() const;
-		float getY() const;
-
-		//Setters
-		void setX(float x);
-		void setY(float y);
-
+		int X() const;
+		int Y() const;
 
 	private:
 		//X and Y coordinates
-		float m_x;
-		float m_y;
+		int m_x;
+		int m_y;
 	};
 
 
