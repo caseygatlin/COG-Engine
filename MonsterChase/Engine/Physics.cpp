26177081715c6fdecb8e9ea8ffe49f60ca124334
Casey Gatlin <u1274838@utah.edu>
2019-11-17 01:26:01 -0700

    Increased readability of Physics

--- MonsterChase/Engine/Physics.cpp
+++ MonsterChase/Engine/Physics.cpp
@@ -4,19 +4,23 @@
 namespace Engine
 {
 
-
 	//Namespace for all classes and methods related to physics
 	namespace Physics
 	{
+
 		//Checks the collision of a player and an enemy
 		//Player loses health if there is a collision
 		bool checkCollision(GameObject* player, GameObject* enemy)
 		{
+
 			if (player->getPosition() == enemy->getPosition())
 			{
+
 				player->ReduceHealth();
 				return true;
+
 			}
+
 			return false;
 		}
 	}
--- MonsterChase/Engine/Physics.h
+++ MonsterChase/Engine/Physics.h
@@ -5,11 +5,13 @@
 
 namespace Engine
 {
+
 	class GameObject;
 
 	//Namespace for all classes and methods related to physics
 	namespace Physics
 	{
+
 		//Takes away player health and returns true if collided
 		//false otherwise
 		bool checkCollision(GameObject* player, GameObject* enemy);
