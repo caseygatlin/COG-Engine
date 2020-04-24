1861f06acadaa37eb5037d385c5e9aa7fadadb32
Casey Gatlin <u1274838@utah.edu>
2020-04-23 23:28:15 -0600

    Added in files and methods for spawning objects with their data

---
+++ MonsterChase/Engine/Spawning/Private/ObjectSpawner.cpp
@@ -0,0 +1,205 @@
+#include "../Public/ObjectSpawner.h"
+#include "../../Containers/Public/Pointers.h"
+#include "../../Containers/Public/Point2D.h"
+#include "../../GameObject/Public/GameObject.h"
+#include "../../External/json.h"
+#include "../../Physics/Public/PhysicsInfo.h"
+#include "../../Physics/Public/Physics.h"
+#include "../../World/Public/World.h"
+#include "../../Graphics/Public/RenderData.h"
+#include "../../Graphics/Public/Graphics.h"
+
+
+#include<assert.h>
+#include<vector>
+#include<string>
+#include<cstring>
+
+namespace Engine
+{
+	namespace ObjectSpawner
+	{
+		void SpawnGameObject(const std::string& i_jsonFileName)
+		{
+			SmartPtr<GameObject> spawnedObject;
+			Physics::PhysicsInfo objectPhysicsInfo;
+			Graphics::RenderData objectRenderData;
+
+			std::vector<uint8_t> jsonFileData = JSONReader::LoadFile(i_jsonFileName);
+
+			if (!jsonFileData.empty())
+			{
+
+				nlohmann::json jsonData = nlohmann::json::parse(jsonFileData);
+				spawnedObject = JSONReader::JSONToGameObject(jsonData);
+
+				World::AddGameObject(spawnedObject);
+
+				objectPhysicsInfo = Physics::PhysicsInfo(
+					World::GetGameObject(World::GetNumGameObjects() - 1),
+					JSONReader::JSONToMass(jsonData),
+					JSONReader::JSONToDrag(jsonData)
+					);
+
+				std::string texturePathStr = JSONReader::JSONToTexture(jsonData);
+
+				char* texturePath = new char[texturePathStr.size() + 1];
+				strcpy(texturePath, texturePathStr.c_str());
+
+				GLib::Sprites::Sprite* texture = Graphics::CreateSprite(texturePath);
+
+				delete[] texturePath;
+
+				objectRenderData = Graphics::RenderData(
+					World::GetGameObject(World::GetNumGameObjects() - 1),
+					texture
+					);
+
+				Physics::AddPhysicsInfo(objectPhysicsInfo);
+				Graphics::AddRenderData(objectRenderData);
+
+			}
+		}
+	}
+
+	namespace JSONReader
+	{
+		SmartPtr<GameObject> JSONToGameObject(nlohmann::json& i_jsonData)
+		{
+
+			Point2D spawnPoint(GetSpawnPointFromJSON(i_jsonData["SpawnPoint"]));
+
+			SmartPtr<GameObject> gameObject = GameObject::CreateGameObject(spawnPoint);
+
+			return gameObject;
+
+		}
+
+		float JSONToMass(nlohmann::json& i_jsonData)
+		{
+			if (i_jsonData.contains("PhysicsInfo"))
+			{
+				if (i_jsonData["PhysicsInfo"].contains("Mass"))
+				{
+					return GetMassFromJSON(i_jsonData["PhysicsInfo"]["Mass"]);
+				}
+			}
+
+			return 0.0F;
+
+		}
+
+		float JSONToDrag(nlohmann::json& i_jsonData)
+		{
+			if (i_jsonData.contains("PhysicsInfo"))
+			{
+				if (i_jsonData["PhysicsInfo"].contains("Drag"))
+				{
+					return GetDragFromJSON(i_jsonData["PhysicsInfo"]["Drag"]);
+				}
+			}
+
+			return 0.0F;
+
+		}
+
+		std::string JSONToTexture(nlohmann::json& i_jsonData)
+		{
+			if (i_jsonData.contains("RenderData"))
+			{
+				if (i_jsonData["RenderData"].contains("Texture"))
+				{
+					return GetTextureFromJSON(i_jsonData["RenderData"]["Texture"]);
+				}
+			}
+
+			return std::string();
+		}
+
+
+		static Point2D GetSpawnPointFromJSON(const nlohmann::json& i_jsonData)
+		{
+			if (i_jsonData.is_array() && i_jsonData.size() == 2)
+			{
+
+				return Point2D(i_jsonData[0], i_jsonData[1]);
+
+			}
+
+			return Point2D(0.0F, 0.0F);
+		}
+
+		float GetMassFromJSON(const nlohmann::json& i_jsonData)
+		{
+			if (i_jsonData.is_number_float())
+			{
+
+				return i_jsonData;
+
+			}
+
+			return 0.0F;
+		}
+
+		float GetDragFromJSON(const nlohmann::json& i_jsonData)
+		{
+			if (i_jsonData.is_number_float())
+			{
+
+				return i_jsonData;
+
+			}
+
+			return 0.0F;
+		}
+
+		std::string GetTextureFromJSON(const nlohmann::json& i_jsonData)
+		{
+			if (i_jsonData.is_string())
+			{
+
+				return i_jsonData;
+
+			}
+
+			return std::string();
+		}
+
+		std::vector<uint8_t> LoadFile(const std::string& i_fileName)
+		{
+			std::vector<uint8_t> fileData;
+
+			if (!i_fileName.empty())
+			{
+				FILE* pFile = nullptr;
+
+				errno_t fopenError = fopen_s(&pFile, i_fileName.c_str(), "rb");
+				if (fopenError == 0)
+				{
+					if (pFile != nullptr)
+					{
+						int FileIOError = fseek(pFile, 0, SEEK_END);
+						assert(FileIOError == 0);
+
+						long FileSize = ftell(pFile);
+						assert(FileSize >= 0);
+
+						FileIOError = fseek(pFile, 0, SEEK_SET);
+						assert(FileIOError == 0);
+
+						fileData.reserve(FileSize);
+						fileData.resize(FileSize);
+
+						size_t FileRead = fread(&fileData[0], 1, FileSize, pFile);
+						assert(FileRead == FileSize);
+
+						fclose(pFile);
+					}
+				}
+			}
+
+			return fileData;
+		}
+
+	}
+}
---
+++ MonsterChase/Engine/Spawning/Public/ObjectSpawner.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "../../GameObject/Public/GameObject.h"
+#include "../../Containers/Public/Point2D.h"
+#include "../../Containers/Public/Pointers.h"
+#include "../../Physics/Public/PhysicsInfo.h"
+#include "../../External/json.h"
+
+#include <assert.h>
+#include <vector>
+
+namespace Engine
+{
+	namespace ObjectSpawner
+	{
+
+		void SpawnGameObject(const std::string& i_jsonFileName);
+
+	}
+
+	namespace JSONReader
+	{
+
+		SmartPtr<GameObject> JSONToGameObject(nlohmann::json& i_jsonData);
+
+		float JSONToMass(nlohmann::json& i_jsonData);
+		float JSONToDrag(nlohmann::json& i_jsonData);
+		std::string JSONToTexture(nlohmann::json& i_jsonData);
+
+		static Point2D GetSpawnPointFromJSON(const nlohmann::json& i_jsonData);
+
+		static float GetMassFromJSON(const nlohmann::json& i_jsonData);
+
+		static float GetDragFromJSON(const nlohmann::json& i_jsonData);
+
+		static std::string GetTextureFromJSON(const nlohmann::json& i_jsonData);
+
+		std::vector<uint8_t> LoadFile(const std::string& i_fileName);
+
+	}
+}
