900769f56c85e61933d1ad9fbad40b9078d0a1f2
Casey Gatlin <u1274838@utah.edu>
2020-05-06 23:27:10 -0600

    Added in functions for spawning in Collideables from a JSON file

--- MonsterChase/Engine/Spawning/Private/ObjectSpawner.cpp
+++ MonsterChase/Engine/Spawning/Private/ObjectSpawner.cpp
@@ -8,6 +8,9 @@
 #include "../../World/Public/World.h"
 #include "../../Graphics/Public/RenderData.h"
 #include "../../Graphics/Public/Graphics.h"
+#include "../../Physics/Collision/Public/Collision.h"
+#include "../../Physics/Collision/Public/Collideable.h"
+#include "../../Physics/Collision/Public/AABB.h"
 
 
 #include<assert.h>
@@ -69,6 +72,61 @@ namespace Engine
 			}
 		}
 
+		void SpawnCollideable(const std::string& i_jsonFileName)
+		{
+			SmartPtr<GameObject> spawnedObject;
+			Physics::PhysicsInfo objectPhysicsInfo;
+			Graphics::RenderData objectRenderData;
+			SmartPtr<Physics::Collideable> spawnedCollideable;
+
+			std::vector<uint8_t> jsonFileData = JSONReader::LoadFile(i_jsonFileName);
+
+			if (!jsonFileData.empty())
+			{
+
+				nlohmann::json jsonData = nlohmann::json::parse(jsonFileData);
+				spawnedObject = JSONReader::JSONToGameObject(jsonData);
+
+				World::AddGameObject(spawnedObject);
+
+				spawnedCollideable = Physics::Collideable::CreateCollideable(
+					World::GetGameObject(World::GetNumGameObjects() - 1),
+					JSONReader::JSONToAABB(jsonData)
+					);
+
+				Physics::AddCollideable(spawnedCollideable);
+
+				std::string controllerName = JSONReader::JSONToController(jsonData);
+				if (!controllerName.empty())
+				{
+					Controllers.insert({ controllerName, World::GetGameObject(World::GetNumGameObjects() - 1) });
+				}
+
+				objectPhysicsInfo = Physics::PhysicsInfo(
+					World::GetGameObject(World::GetNumGameObjects() - 1),
+					JSONReader::JSONToMass(jsonData),
+					JSONReader::JSONToDrag(jsonData)
+				);
+
+				std::string texturePathStr = JSONReader::JSONToTexture(jsonData);
+
+				char* texturePath = new char[texturePathStr.size() + 1];
+				strcpy_s(texturePath, texturePathStr.size() + 1, texturePathStr.c_str());
+
+				GLib::Sprites::Sprite* texture = Graphics::CreateSprite(texturePath);
+
+				delete[] texturePath;
+
+				objectRenderData = Graphics::RenderData(
+					World::GetGameObject(World::GetNumGameObjects() - 1),
+					texture
+				);
+
+				Physics::AddPhysicsInfo(objectPhysicsInfo);
+				Graphics::AddRenderData(objectRenderData);
+			}
+		}
+
 		void ClearControllers()
 		{
 
@@ -82,12 +140,30 @@ namespace Engine
 		SmartPtr<GameObject> JSONToGameObject(nlohmann::json& i_jsonData)
 		{
 
-			Point2D spawnPoint(GetSpawnPointFromJSON(i_jsonData["SpawnPoint"]));
+			Point2D spawnPoint(GetPoint2DFromJSON(i_jsonData["SpawnPoint"]));
 
 			SmartPtr<GameObject> gameObject = GameObject::CreateGameObject(spawnPoint);
 
 			return gameObject;
 
+		}
+
+		Physics::AABB JSONToAABB(nlohmann::json& i_jsonData)
+		{
+
+			if (i_jsonData.contains("Collision") &&
+				i_jsonData["Collision"].contains("Bounding Box") &&
+				i_jsonData["Collision"]["Bounding Box"].contains("Extents") &&
+				i_jsonData["Collision"]["Bounding Box"].contains("Center"))
+			{
+				Point2D center = GetPoint2DFromJSON(i_jsonData["Collision"]["Bounding Box"]["Center"]);
+				Point2D extents = GetPoint2DFromJSON(i_jsonData["Collision"]["Bounding Box"]["Extents"]);
+				
+				return Physics::AABB(center, extents);
+
+			}
+
+			return Physics::AABB();
 		}
 
 		float JSONToMass(nlohmann::json& i_jsonData)
@@ -96,7 +172,7 @@ namespace Engine
 			{
 				if (i_jsonData["PhysicsInfo"].contains("Mass"))
 				{
-					return GetMassFromJSON(i_jsonData["PhysicsInfo"]["Mass"]);
+					return GetFloatFromJSON(i_jsonData["PhysicsInfo"]["Mass"]);
 				}
 			}
 
@@ -110,7 +186,7 @@ namespace Engine
 			{
 				if (i_jsonData["PhysicsInfo"].contains("Drag"))
 				{
-					return GetDragFromJSON(i_jsonData["PhysicsInfo"]["Drag"]);
+					return GetFloatFromJSON(i_jsonData["PhysicsInfo"]["Drag"]);
 				}
 			}
 
@@ -124,7 +200,7 @@ namespace Engine
 			{
 				if (i_jsonData["RenderData"].contains("Texture"))
 				{
-					return GetTextureFromJSON(i_jsonData["RenderData"]["Texture"]);
+					return GetStringFromJSON(i_jsonData["RenderData"]["Texture"]);
 				}
 			}
 
@@ -138,7 +214,7 @@ namespace Engine
 			{
 				if (i_jsonData["Controller"].contains("Type"))
 				{
-					return GetTextureFromJSON(i_jsonData["Controller"]["Type"]);
+					return GetStringFromJSON(i_jsonData["Controller"]["Type"]);
 				}
 			}
 
@@ -146,8 +222,33 @@ namespace Engine
 
 		}
 
+		float GetFloatFromJSON(const nlohmann::json& i_jsonData)
+		{
+			if (i_jsonData.is_number_float())
+			{
 
-		static Point2D GetSpawnPointFromJSON(const nlohmann::json& i_jsonData)
+				return i_jsonData;
+
+			}
+
+			return 0.0F;
+
+		}
+
+		std::string GetStringFromJSON(const nlohmann::json& i_jsonData)
+		{
+			if (i_jsonData.is_string())
+			{
+
+				return i_jsonData;
+
+			}
+
+			return std::string();
+
+		}
+
+		Point2D GetPoint2DFromJSON(const nlohmann::json& i_jsonData)
 		{
 			if (i_jsonData.is_array() && i_jsonData.size() == 2)
 			{
@@ -157,59 +258,23 @@ namespace Engine
 			}
 
 			return Point2D(0.0F, 0.0F);
-
 		}
 
-		float GetMassFromJSON(const nlohmann::json& i_jsonData)
+		Matrix4 GetMatrix4FromJSON(const nlohmann::json& i_jsonData)
 		{
-			if (i_jsonData.is_number_float())
+			if (i_jsonData.is_array() && i_jsonData.size() == 16)
 			{
 
-				return i_jsonData;
+				return Matrix4(
+					i_jsonData[0], i_jsonData[1], i_jsonData[2], i_jsonData[3],
+					i_jsonData[4], i_jsonData[5], i_jsonData[6], i_jsonData[7],
+					i_jsonData[8], i_jsonData[9], i_jsonData[10], i_jsonData[11],
+					i_jsonData[12], i_jsonData[13], i_jsonData[14], i_jsonData[15]
+				);
 
 			}
 
-			return 0.0F;
-
-		}
-
-		float GetDragFromJSON(const nlohmann::json& i_jsonData)
-		{
-			if (i_jsonData.is_number_float())
-			{
-
-				return i_jsonData;
-
-			}
-
-			return 0.0F;
-
-		}
-
-		std::string GetTextureFromJSON(const nlohmann::json& i_jsonData)
-		{
-			if (i_jsonData.is_string())
-			{
-
-				return i_jsonData;
-
-			}
-
-			return std::string();
-
-		}
-
-		std::string GetControllerFromJSON(const nlohmann::json& i_jsonData)
-		{
-			if (i_jsonData.is_string())
-			{
-
-				return i_jsonData;
-
-			}
-
-			return std::string();
-
+			return Matrix4::IdentityMatrix;
 		}
 
 		std::vector<uint8_t> LoadFile(const std::string& i_fileName)
--- MonsterChase/Engine/Spawning/Public/ObjectSpawner.h
+++ MonsterChase/Engine/Spawning/Public/ObjectSpawner.h
@@ -4,6 +4,8 @@
 #include "../../Containers/Public/Point2D.h"
 #include "../../Containers/Public/Pointers.h"
 #include "../../Physics/Public/PhysicsInfo.h"
+#include "../../Physics/Collision/Public/AABB.h"
+#include "../../Containers/Public/Matrix4.h"
 #include "../../External/json.h"
 
 #include <assert.h>
@@ -16,6 +18,8 @@ namespace Engine
 
 		void SpawnGameObject(const std::string& i_jsonFileName);
 
+		void SpawnCollideable(const std::string& i_jsonFileName);
+
 		void ClearControllers();
 
 	}
@@ -25,6 +29,8 @@ namespace Engine
 
 		SmartPtr<GameObject> JSONToGameObject(nlohmann::json& i_jsonData);
 
+		Physics::AABB JSONToAABB(nlohmann::json& i_jsonData);
+
 		float JSONToMass(nlohmann::json& i_jsonData);
 
 		float JSONToDrag(nlohmann::json& i_jsonData);
@@ -33,15 +39,13 @@ namespace Engine
 
 		std::string JSONToController(nlohmann::json& i_jsonData);
 
-		static Point2D GetSpawnPointFromJSON(const nlohmann::json& i_jsonData);
+		static float GetFloatFromJSON(const nlohmann::json& i_jsonData);
 
-		static float GetMassFromJSON(const nlohmann::json& i_jsonData);
+		static std::string GetStringFromJSON(const nlohmann::json& i_jsonData);
 
-		static float GetDragFromJSON(const nlohmann::json& i_jsonData);
+		static Point2D GetPoint2DFromJSON(const nlohmann::json& i_jsonData);
 
-		static std::string GetTextureFromJSON(const nlohmann::json& i_jsonData);
-
-		static std::string GetControllerFromJSON(const nlohmann::json& i_jsonData);
+		static Matrix4 GetMatrix4FromJSON(const nlohmann::json& i_jsonData);
 
 		std::vector<uint8_t> LoadFile(const std::string& i_fileName);
 
