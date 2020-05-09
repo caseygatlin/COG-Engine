60e0639b5c2ba94b86bdfed83fc3326731857def
Casey Gatlin <u1274838@utah.edu>
2020-05-08 22:35:55 -0600

    HealthComponent allows using HitBuffer for temporary invincibility

--- MonsterChase/Engine/Components/Public/HealthComponent-inl.h
+++ MonsterChase/Engine/Components/Public/HealthComponent-inl.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "HealthComponent.h"
 #include "../../GameObject/Public/GameObject.h"
+#include "../../Console/Public/ConsolePrint.h"
 #include "ComponentType.h"
 
 
@@ -8,7 +9,18 @@ namespace Engine
 {
 	inline void HealthComponent::Update(GameObject* i_gameObject, const float& i_dt)
 	{
+		if (m_TookDamage)
+		{
+			m_CurrentHitBufferTime -= i_dt;
+		}
 
+		if (m_CurrentHitBufferTime < 0.0f)
+		{
+
+			m_CurrentHitBufferTime = m_MaxHitBufferTime;
+			m_TookDamage = false;
+
+		}
 	}
 
 	inline bool HealthComponent::IsComponentType(const ComponentType& i_componentType) const
@@ -25,10 +37,17 @@ namespace Engine
 
 	inline bool HealthComponent::TakeDamage()
 	{
+		DEBUG_PRINT("Health: %d\n", m_Health);
 
-		if (--m_Health <= 0)
-		{ 
-			return true; 
+		if (!m_TookDamage)
+		{
+			m_TookDamage = true;
+
+			if (--m_Health <= 0)
+			{
+				m_Health = 0;
+				return true;
+			}
 		}
 
 		return false;
@@ -46,6 +65,12 @@ namespace Engine
 	{
 
 		m_Health = i_Health;
+
+	}
+	inline void HealthComponent::SetMaxHitBufferTime(const float& i_MaxTime)
+	{
+
+		m_MaxHitBufferTime = i_MaxTime;
 
 	}
 }
