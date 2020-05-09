71daf1a5b26a4eea79cd730a0f8d18751f74d720
Casey Gatlin <u1274838@utah.edu>
2020-05-08 22:58:21 -0600

    Game class includes Spawners and GameOver as members

--- MonsterChase/Gamecode/Game/Public/Game.h
+++ MonsterChase/Gamecode/Game/Public/Game.h
@@ -1,13 +1,28 @@
 #pragma once
 
+#include "../../Asteroids/Public/AsteroidSpawner.h"
+#include "../../Player Ship/Public/PlayerShipSpawner.h"
+#include "../Game Over/Public/GameOverScreen.h"
+
 #include <Windows.h>
 #include <vector>
 
 class Game
 {
 public:
+    Game() :
+        m_AsteroidSpawner(this),
+        m_PlayerShipSpawner(this),
+        m_GameOverScreen()
+    {}
 
     void Init();
     void ShutDown();
+
+private:
+
+    AsteroidSpawner m_AsteroidSpawner;
+    PlayerShipSpawner m_PlayerShipSpawner;
+    GameOverScreen m_GameOverScreen;
 	
 };
