b9f12a7a09dfef14f3841e00201944ad6a87092d
Casey Gatlin <u1274838@utah.edu>
2020-05-08 22:29:08 -0600

    Added GetComponentType to component interface

--- MonsterChase/Engine/Components/Public/IGOComponent.h
+++ MonsterChase/Engine/Components/Public/IGOComponent.h
@@ -15,6 +15,7 @@ namespace Engine
 		virtual void OnAttach(GameObject* i_gameObject) = 0;
 		virtual	void Update(GameObject* i_gameObject) = 0;
 		virtual	bool IsComponentType(const ComponentType& i_componentType)	const = 0;
+		virtual ComponentType GetComponentType() const = 0;
 
 	};
 
