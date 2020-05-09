9a826c75159ec337e4497f89e3ac9d041423f4ee
Casey Gatlin <u1274838@utah.edu>
2020-05-08 22:29:48 -0600

    Added dt parameter in Component Interface Update()

--- MonsterChase/Engine/Components/Public/IGOComponent.h
+++ MonsterChase/Engine/Components/Public/IGOComponent.h
@@ -13,7 +13,7 @@ namespace Engine
 
 		virtual ~IGOComponent() {}
 		virtual void OnAttach(GameObject* i_gameObject) = 0;
-		virtual	void Update(GameObject* i_gameObject) = 0;
+		virtual	void Update(GameObject* i_gameObject, const float& i_dt) = 0;
 		virtual	bool IsComponentType(const ComponentType& i_componentType)	const = 0;
 		virtual ComponentType GetComponentType() const = 0;
 
