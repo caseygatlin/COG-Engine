d708d3dccfddf4a17d2e4abf60e429c73b3cfe23
Casey Gatlin <u1274838@utah.edu>
2020-05-08 22:59:51 -0600

    PlayerShipSpawner adds HealthComponent

--- MonsterChase/Gamecode/Player Ship/Private/PlayerShipSpawner.cpp
+++ MonsterChase/Gamecode/Player Ship/Private/PlayerShipSpawner.cpp
@@ -1,28 +1,44 @@
 
-#include "PlayerShipSpawner.h"
+#include "../Public/PlayerShipSpawner.h"
 
+#include "../../Game/Public/Game.h"
 #include "Console/Public/ConsolePrint.h"
 #include "JobSystem/Public/JobSystem.h"
 #include "Spawning/Public/ObjectSpawner.h"
+#include "Components/Public/ComponentType.h"
+#include "Components/Public/PlayerControllerComponent.h"
+#include "Components/Public/HealthComponent.h"
+#include "Components/Public/IGOComponent.h"
 #include "Physics/Collision/Public/Collision.h"
 
 #include <Windows.h>
 
 void PlayerShipSpawner::SpawnShip()
 {
+	using namespace Engine;
+
 	Sleep(1000);
 
-	Engine::ObjectSpawner::SpawnCollideable("Content\\PlayerShipData.json");
-	Engine::Physics::GetCollideable(0).Acquire()->AssignCallback(std::bind(&PlayerShipSpawner::OnCollision, this));
-
-	while (Engine::JobSystem::HasJobs("Default"))
+	SmartPtr<Physics::Collideable> playerShipCollideable;
+	if (ObjectSpawner::SpawnCollideable("Content\\PlayerShipData.json", playerShipCollideable))
 	{
-		Sleep(10);
-	}
 
-	if (!Engine::JobSystem::ShutdownRequested())
-	{
-		Engine::JobSystem::RequestShutdown();
+		SmartPtr<GameObject> playerShipObject = playerShipCollideable->GetGameObject().Acquire();
+
+		if (playerShipObject->HasComponent(ComponentType::PLAYER_INPUT))
+		{
+
+			SmartPtr<IGOComponent> healthComponentBase = CreateComponent<HealthComponent>();
+			playerShipObject->Attach(healthComponentBase);
+
+			SmartPtr<HealthComponent> healthComponent = healthComponentBase;
+
+			healthComponent->SetHealth(1);
+			healthComponent->SetMaxHitBufferTime(1000);
+
+			playerShipCollideable->AssignCallback(std::bind(&PlayerShipSpawner::OnCollision, this));
+
+		}
 	}
 }
 
--- MonsterChase/Gamecode/Player Ship/Public/PlayerShipSpawner.h
+++ MonsterChase/Gamecode/Player Ship/Public/PlayerShipSpawner.h
@@ -1,11 +1,22 @@
 #pragma once
 
+#include "Containers/Public/Pointers.h"
+#include "Components/Public/HealthComponent.h"
+
+class Game;
+
 class PlayerShipSpawner
 {
 public:
+	PlayerShipSpawner(Game* i_GameInstance) :
+		m_GameInstance(i_GameInstance)
+	{}
 
 	void SpawnShip();
 
 	void OnCollision();
+
+private:
+	Game* m_GameInstance;
 
 };
