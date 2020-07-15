1fbe524c2d2a119eb2fe014f430a4153a77c239b
Casey Gatlin <32660336+caseygatlin@users.noreply.github.com>
2020-07-15 06:01:18 -0600

    Added documentation for AsteroidSpawner.cpp

--- MonsterChase/Gamecode/Asteroids/Private/AsteroidSpawner.cpp
+++ MonsterChase/Gamecode/Asteroids/Private/AsteroidSpawner.cpp
@@ -16,31 +16,42 @@ void AsteroidSpawner::SpawnAsteroids()
 
 	using namespace Engine;
 
+	// Give time for window and player to initialize.
 	Sleep(2000);
 
+	// Initialize number of asteroids.
 	size_t numAsteroids = 0;
 
+	// Continuously spawn asteroids until signaled otherwise.
 	while (m_bContinueSpawning)
 	{
+		// Wait for one second between spawning asteroids.
 		Sleep(1000);
-
 		numAsteroids++;
 
+		// Create a SmartPtr of the asteroid.
 		SmartPtr<Physics::Collideable> asteroidCollideable;
+		
+		// Call the ObjectSpawner to spawn a collideable based on given JSON data.
 		ObjectSpawner::SpawnCollideable("Content\\AsteroidData.json", asteroidCollideable);
+		
+		// Assign a collision callback to the asteroid using std::bind.
 		asteroidCollideable->AssignCallback(std::bind(&AsteroidSpawner::OnCollision, this));
 
 		SmartPtr<GameObject> asteroid = asteroidCollideable->GetGameObject().Acquire();
 
+		// Create a Random Spawn component
 		SmartPtr<IGOComponent> randomSpawnComponentBase = CreateComponent<RandomSpawnComponent>();
 		SmartPtr<RandomSpawnComponent> randomSpawnComponent = randomSpawnComponentBase;
 
+		// Set spawn variables based on current asteroid count.
 		if (numAsteroids % 2 == 0)
 		{
 
 			randomSpawnComponent->SetMaxMin(390, -390, 298, 299);
 
 		}
+		
 		else
 		{
 
@@ -48,24 +59,30 @@ void AsteroidSpawner::SpawnAsteroids()
 
 		}
 
+		// Attach the Random Spawn component to the asteroid created earlier.
 		asteroid->Attach(randomSpawnComponentBase);
 
 
+		// Set the movement direction of the asteroid based on its spawned position.
 		Point2D asteroidPosition = asteroid->GetPosition();
 
 		float asteroidDirX = (asteroidPosition.X() > 0) ? -1.0f : 1.0f;
 		float asteroidDirY = (asteroidPosition.Y() > 0) ? -1.0f : 1.0f;
 
 		asteroid->ChangeDir(Point2D(asteroidDirX, asteroidDirY));
+		
 	}
 }
 
 void AsteroidSpawner::StopSpawning()
 {
+	
 	m_bContinueSpawning = false;
+	
 }
 
 void AsteroidSpawner::OnCollision()
 {
-
+	
+	// Action to be performed on collision should be added here.
 }
