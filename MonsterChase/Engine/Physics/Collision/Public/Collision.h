9a1504c6a24fb7a59164956106619d99b4a55ebd
Casey Gatlin <32660336+caseygatlin@users.noreply.github.com>
2020-07-15 06:09:08 -0600

    Documentation for Collision.h

--- MonsterChase/Engine/Physics/Collision/Public/Collision.h
+++ MonsterChase/Engine/Physics/Collision/Public/Collision.h
@@ -9,15 +9,28 @@ namespace Engine
 	namespace Physics
 	{
 
+		// Adds a collideable to  the collideables list.
 		void AddCollideable(SmartPtr<Collideable>& i_Collideable);
+		
+		// Removes a collideable from the collideables list given a Collideable.
 		void RemoveCollideable(WeakPtr<Collideable>& i_Collideable);
+		
+		// Removes a collideable from the collideables list given a GameObject.
 		void RemoveCollideable(WeakPtr<GameObject>& i_GameObject);
 
+		// Finds a possible collision between two collideables.
 		bool FindCollision(const float& i_dt, WeakPtr<Collideable>& o_Object1, WeakPtr<Collideable>& o_Object2);
+		
+		// Checks whether a collision occurred between two collideables using Swept Separating Axis.
 		bool CheckCollision(const float& i_dt, WeakPtr<Collideable> i_Object1, WeakPtr<Collideable> i_Object2);
 
+		// Returns a WeakPtr to a collideable in the collideables list at a given index.
 		WeakPtr<Collideable> GetCollideable(const size_t& i_index);
+		
+		// Returns the current size of the collideables list.
 		size_t GetNumCollideables();
+		
+		// Empties the collideables list.
 		void Destroy();
 
 	}
