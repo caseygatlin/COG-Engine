908411d42a96774187041b13b92a712d45e668b1
Casey Gatlin <32660336+caseygatlin@users.noreply.github.com>
2020-07-18 04:03:07 -0600

    Documentation and readability for ComponentType.h

--- MonsterChase/Engine/Components/Public/ComponentType.h
+++ MonsterChase/Engine/Components/Public/ComponentType.h
@@ -2,12 +2,14 @@
 
 namespace Engine
 {
+	
+	// Enum for differentiating the different types of attachable components.
 	enum class ComponentType
 	{
 
-		HEALTH,
-		PLAYER_INPUT,
-		RANDOM_SPAWN,
+		HEALTH,			// Health Component - a health system for GameObjects.
+		PLAYER_INPUT,		// Player Controller Component - handles player input for a GameObject.
+		RANDOM_SPAWN,		// Random Spawn Component - Randomly generates spawn position for a GameObject.
 
 	};
 }
